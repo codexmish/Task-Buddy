37982751dd6af8c434de9f4e37bdde2041a2220c
codexmish <majharulislam.shanto.70@gmail.com>
2026-01-12 19:50:27 +0600

    file stracture ready

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,21 @@
 import React from 'react'
 import './App.css'
+import TaskForm from './components/TaskForm'
+import TaskList from './components/TaskList'
+import ProgressTracker from './components/ProgressTracker'
 
 const App = () => {
   return (
-    <div>App</div>
+    <>
+    <header>
+      <h1>TaskBuddy</h1>
+      <p>Your friendly task Manager</p>
+    </header>
+    <TaskForm/>
+    <TaskList/>
+    <ProgressTracker/>
+    
+    </>
   )
 }
 
